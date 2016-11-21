a09c53bb4d476e0923151944eec62acc33835d9e
Robins Gupta <robinskumar73@gmail.com>
2016-11-21 22:34:02 +0530

    First commit

---
+++ client/scripts/controllers/danzHome.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module($snaphy.getModuleName())
+
+//Controller for danzHomeControl ..
+.controller('danzHomeControl', ['$scope', '$stateParams', 'Database',
+    function($scope, $stateParams, Database) {
+        //Checking if default templating feature is enabled..
+        var defaultTemplate = $snaphy.loadSettings('danzHome', "defaultTemplate");
+        $snaphy.setDefaultTemplate(defaultTemplate);
+        //Use Database.getDb(pluginName, PluginDatabaseName) to get the Database Resource.
+    }//controller function..
+]);
---
+++ readMe.md
@@ -0,0 +1,14 @@
+# danzHome plugin for Snaphy
+
+
+###Danz Foods Home Page
+
+###This plugin is exposed on  `/danzHome` route
+
+###Please copy the` danzHome` folder to `common/settings/` after plugin installed.
+
+### TO Install a npm module use `npm install moduleName --prefix ../../../ --save` and then save the module in package.json of plugin file.
+
+
+####Written by Robins Gupta
+
